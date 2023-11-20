204ab98240307e9253782757de31d9821343f4ec
GeorgetaV <148208360+GeorgetaV@users.noreply.github.com>
2023-11-21 00:28:21 +0200

    Homework 7
    HM

---
+++ HomeWork/Homework 7/Script/script.js
@@ -0,0 +1,95 @@
+//1.
+
+// function num(a, b) {
+//   if (a < b) {
+//     return a
+//   } else {
+//       return b
+//     }
+// }
+  
+// let minNum = num(3, 4)
+// console.log(minNum);
+
+
+// 2. 
+
+  
+// function num1(a, b) {
+//   if (a % 2 === 0) {
+//     console.log(a);
+//   }
+//   if (b % 2 === 0) {
+//     console.log(b)
+//   }
+  
+//   let min = a
+//   let max = b
+//   if (a > b) {
+//     min = b
+//     max = a
+//   }
+
+//   for (let i = max; i >= min; i--) {
+//     num1.push(i)
+//   }
+//   console.log(num1);
+// }
+
+// 3.
+
+// function power(a, b = 2) {
+//   console.log(a**b)
+// }
+
+// power(3)
+
+
+
+// 4. 
+
+
+// function num2(a, b, c, n) {
+//   let numSum = 0
+//   for (let i = 0; i < num2.length; i++) {
+//     numSum += num2[i];
+//   }
+  
+//   return num2
+// }
+
+// let result = num2(1, 2, 3, 4)
+// console.log(result);
+
+
+
+// 5. 
+
+// function num3(n, m) {
+//   let evenSum = 0
+//   let oddSum = 0
+
+//   for(let i = 0; i < num3.length; i++) {
+//   if (n % 2 === 0) {
+//     evenSum += n
+//   } else if (m % 2 === 0) {
+//     oddSum += m
+//   }
+// }
+
+//   console.log(evenSum);
+//   console.log(oddSum);
+// }
+
+
+
+
+// 6. 
+
+// function isArray(arr) {
+//   for(let i = word.length - 1; i >= 0; i--) {
+    
+//   }
+// }
+
+// console.log(arr(["one", 'two', 'three', 'four', 'five' ]));
